80c4b8364656b0b138cb78c4a6867bd5751c9a8d
Vetrichelvan <pythonhubdev@gmail.com>
2024-12-23 16:40:00 +0530

    feat(yazi): added yazi config and few more improvements

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
 
 config.enable_tab_bar = false
 
--- .zshrc
+++ .zshrc
@@ -107,3 +107,6 @@ setopt share_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
 setopt hist_verify
+
+# ---- Zoxide (better cd) -----
+alias cd="z"
